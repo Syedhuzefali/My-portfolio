6351ca7a060daccce14cf450138535d605bcb6fe
Syedhuzefali <106540974+Syedhuzefali@users.noreply.github.com>
2025-06-17 01:43:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import About from "./pages/About";
